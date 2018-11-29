1a58ef8198c979bb05a38cf80ca40cf1f1f8e7e2
Rokec16 <rokficko6@gmail.com>
2018-11-29 15:19:10 +0100

    Version 0.01RF
    delaaaaaaaaa (nekaj)

--- aktuator_ems.py
+++ aktuator_ems.py
@@ -1,70 +1,77 @@
 from pyModbusTCP.client import ModbusClient
-#import mysql.connector
-#from mysql.connector import errorcode
+import mysql.connector
+from mysql.connector import errorcode
 import requests
+import time
+
 
 def modbus_devices(register, value):
-    c=ModbusClient(host="164.8.11.147", port=502, auto_open=True)
-    #regs=c.read_holding_registers(33)
-    #print(regs)
-    regs=c.write_single_register(register,value)
-    regs1=c.read_holding_registers(register)
+    c = ModbusClient(host="164.8.11.147", port=502, auto_open=True)
+    # regs=c.read_holding_registers(33)
+    # print(regs)
+    regs = c.write_single_register(register, value)
+    regs1 = c.read_holding_registers(register)
     print(regs1)
 
-def klima(id,value):
+
+def klima(id, value):
     url = "http://164.8.11.12" + id + "/" + value
     response = requests.post(url)
     print(response)
 
+
 def bojler(value):
-    url="http://164.8.11.128/"+value
-    response=requests.post(url)
+    url = "http://164.8.11.128/" + value
+    response = requests.post(url)
     print(response)
 
-# def sql_read(self):
-#     try:
-#         config= {
-#             'user' : 'emspowerlab',
-#             'password' : 'powerlab2017',
-#             'host' : '164.8.11.146',
-#             'database' : 'ems',
-#             'raise_on_warnings' : True
-#         }
-#         cnx=mysql.connector.connect(**config)
-#     except mysql.connector.Error as err:
-#         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-#             print("Something is wrong with your user name or password")
-#         elif err.errno == errorcode.ER_BAD_DB_ERROR:
-#             print("Database does not exist")
-#         else:
-#             print(err)
-#     else:
-#         cursor = cnx.cursor()
-#         query = ("SELECT * FROM ems_history order by datetime DESC LIMIT 1")
-#     return()
-#
-# def sql_write(self):
-#     return()
 
-if __name__=="__main__":
-    while True:
-        x=sql_read()
+def sql_read():
 
-        if x[0] == 1:
-            if x[1]=='Q':
-                modbus_devices()
-            elif x[1]=='C':
-                klima()
-            elif x[1]=='B':
-                bojler()
-            else:
-                sql_write('Ni šlo blabla ')
-        else:
-            continue
+    config = {
+        'user': 'emspowerlab',
+        'password': 'powerlab2017',
+        'host': '164.8.11.146',
+        'port': '3306',
+        'database': 'ems',
+        'raise_on_warnings': False
+    }
 
-    #klima(3,'ON')
-    #bojler('ON')
-    #modbus_devices(33,3)
+    cnx = mysql.connector.connect(**config)
+    cursor = cnx.cursor(buffered=True)
 
+    query = ("SELECT activity, value FROM ems_history WHERE datetime > NOW() - INTERVAL 1 MINUTE AND id_sender = 1 AND id_receiver = 2")
 
+    cursor.execute(query)
 
+    frame = cursor.fetchall()
+
+    print(frame)
+
+    cursor.close()
+    cnx.close()
+
+def sql_write(self):
+    return()
+
+if __name__ == "__main__":
+    sql_read()
+
+    # while True:
+    #     x = sql_read()
+    #
+    #     if x[0] == 1:
+    #         if x[1] == 'Q':
+    #             modbus_devices()
+    #         elif x[1] == 'C':
+    #             klima()
+    #         elif x[1] == 'B':
+    #             bojler()
+    #         else:
+    #             sql_write('Ni šlo blabla ')
+    #     else:
+    #         continue
+
+    # klima(3,'ON')
+    # bojler('ON')
+    # modbus_devices(33,3)
